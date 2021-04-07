4a1cb7322a5240ab61b49874c34c1150d5198109
janerist <janerist@fastmail.com>
2021-04-07 07:55:16 +0200

    Upgrade chart.js

--- web/src/app/jumpers/detail/jumper-form.component.ts
+++ web/src/app/jumpers/detail/jumper-form.component.ts
@@ -1,8 +1,28 @@
 import {Component, Input, OnChanges} from '@angular/core';
-import {Chart} from 'chart.js';
 import {Router} from '@angular/router';
 import {JumperActivityResponseModel} from '../../shared/api-responses';
 import * as moment from 'moment';
+
+import {
+  Chart,
+  LineElement,
+  PointElement,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip
+} from 'chart.js';
+
+Chart.register(
+  LineElement,
+  PointElement,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip
+);
 
 @Component({
   selector: 'app-jumper-form',
@@ -21,10 +41,12 @@ export class JumperFormComponent implements OnChanges {
   }
 
   handleClick(event) {
-    const activePoints = this.chart ? this.chart.getElementsAtEvent(event) : [];
+    const activePoints = this.chart
+      ? this.chart.getElementsAtEventForMode(event, 'nearest', {intersect: true}, false)
+      : [];
+
     if (activePoints.length === 1) {
-      // tslint:disable-next-line:no-string-literal
-      const index = activePoints[0]['_index'];
+      const index = activePoints[0].index;
       this.router.navigate(['/tournaments', this.rankings[index].tournamentId]);
     }
   }
@@ -51,43 +73,43 @@ export class JumperFormComponent implements OnChanges {
           borderColor: 'rgb(54, 162, 235)',
           data: this.rankings.map(r => r.rank),
           fill: false,
-          pointHitRadius: 10,
+          pointHitRadius: 10
         }]
       },
       options: {
         responsive: true,
-        title: {
-          display: true,
-          text: 'Hover over the data points for more information.'
+        plugins: {
+          title: {
+            display: true,
+            text: 'Hover over the data points for more information.'
+          },
+          tooltip: {
+            callbacks: {
+              beforeTitle: tooltipItems => {
+                const item = this.rankings[tooltipItems[0].dataIndex];
+                return `${item.tournamentType} (DSJ${item.gameVersion})`;
+              }
+            }
+          }
         },
         scales: {
-          yAxes: [{
+          y: {
+            min: 1,
+            max: Math.max(50, Math.ceil(this.rankings.reduce((m, r) => Math.max(m, r.rank), 0) / 10) * 10),
+            reverse: true,
             ticks: {
-              min: 1,
-              max: Math.max(50, Math.ceil(this.rankings.reduce((m, r) => Math.max(m, r.rank), 0) / 10) * 10),
-              stepSize: 10,
-              reverse: true
+              stepSize: 10
             },
-            scaleLabel: {
+            title: {
               display: true,
-              labelString: 'Rank'
-            }
-          }]
-        },
-        tooltips: {
-          callbacks: {
-            beforeTitle: tooltipItems => {
-              const item = this.rankings[tooltipItems[0].index];
-              return `${item.tournamentType} (DSJ${item.gameVersion})`;
+              text: 'Rank'
             }
           }
         },
-        hover: {
-          onHover: (e, el) => {
-            $('#jumper-form').css('cursor', el[0] ? 'pointer' : 'default');
-          }
+        onHover: (e, el) => {
+          $('#jumper-form').css('cursor', el[0] ? 'pointer' : 'default');
         }
-      },
+      }
     });
   }
 }
